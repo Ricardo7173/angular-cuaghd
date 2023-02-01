106d7e9d916ec932d3cde22ca3447da89ad71b5c
Omar Ricardo Garay Garcia <riki0303@outlool.es>
2023-02-01 11:44:01 -0600

    Clase 01

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {"path": '', component:AppComponent, pathMatch:"full"},
+  {"path": 'login', component:LoginComponent, pathMatch:"full"},
+  {"path": 'register', component:RegisterComponent, pathMatch:"full"},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
